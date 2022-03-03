b0e7959e43c27f46dce58c11267ee54dfe8984b7
WW <streetpoet@163.com>
2022-03-03 16:39:28 -0600

    add sorting

--- src/main/java/com/spstudio/dashboard/controller/DashBoardController.java
+++ src/main/java/com/spstudio/dashboard/controller/DashBoardController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.text.DecimalFormat;
 import java.time.Instant;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -33,11 +35,13 @@ public class DashBoardController {
             long days = Instant.now().minusSeconds(x.getCreated()).getEpochSecond() / 24 / 3600L;
             dashBoardModel.setBuyDays(String.valueOf(days));
             dashBoardModel.setBuyCount(String.valueOf(x.getBuyCount()));
-            double rate = x.getLatestCbPrice() * 100 / (x.getBasePrice() * x.getPreferredProfit());
+            double rate = x.getLatestCbPrice() * 100 / (x.getBasePrice() * (1.01 + x.getPreferredProfit()));
             dashBoardModel.setCompleteRate(df.format(rate));
             dashBoardModel.setLongTerm(x.getIsLongTerm());
             return dashBoardModel;
-        }).collect(Collectors.toList());
+        }).sorted(Comparator.comparing(DashBoardModel::getCompleteRate)).collect(Collectors.toList());
+        Collections.reverse(products);
+        model.addAttribute("dashboard", products);
         return "dashboard";
     }
 
